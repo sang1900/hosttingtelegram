65fbd41928c08d3efd4f9257ef7123bd1f867e8e
sang1900 <76580837+sang1900@users.noreply.github.com>
2021-12-03 19:58:34 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -9,7 +9,7 @@ from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','upload']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("<b>Vui lòng chờ...!</b>", quote = True)
+    #reply_text = await message.reply_text("<b>Vui lòng chờ...!</b>", quote = True)
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:
