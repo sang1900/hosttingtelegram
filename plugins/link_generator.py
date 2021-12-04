1c9afa2fa09cad62a24eb8a252fcdf774a4482e8
sang1900 <76580837+sang1900@users.noreply.github.com>
2021-12-04 13:30:11 +0700

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -62,14 +62,18 @@ async def link_generator(client: Client, message: Message):
     await channel_message.reply_text(f"✅ LƯU TRỮ THÀNH CÔNG \n\n🔗 Your URL : {link}</b>\n(Vì đây là bản FREE nên cần mở link rút gọn để lấy URL lưu trữ, liên hệ <a href='https://fb.com/sang1900'>Admin</a> để xoá link rút gọn.)", quote=True, reply_markup=reply_markup)
 @Bot.on_message(filters.private & filters.command('upload'))
 async def link_generator(client: Client, message: Message):
-    while True:
-        try:
-            channel_message = await client.ask(text = "Chuyển tiếp 1 tin nhắn hoặc link từ kênh Database (có trích dẫn)\n<code>(Chuyển tiếp cho tôi trong vòng 5 phút)</code>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
-            return
-        msg_id = await get_message_id(client, channel_message)
-        if msg_id:
-            break
+    try:
+        channel_message = await client.ask(text = """Gửi cho tôi tự thứ bạn muốn lưu trữ. Nó có thể là :
+💬 Tin nhắn
+🖼️ Hình ảnh
+🎬 Video
+🔗 Liên kết
+🗂️ Thư mục
+📁 Tệp tin
+...""")
+    except:
+        return
+    msg_id = await get_message_id(client, channel_message)
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
